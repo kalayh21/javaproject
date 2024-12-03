25f4a7dd9d935187a9470297b06c85b3da149447
kalay <kalayh21a@gmail.com>
2024-12-03 21:45:17 +0300

    adding of two numbers

---
+++ Mymethod.java
@@ -0,0 +1,9 @@
+public class Mymethod {
+    public static int addNumbers(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Sum: " + addNumbers(5, 10));
+    }
+}
